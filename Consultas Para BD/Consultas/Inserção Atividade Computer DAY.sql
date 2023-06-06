c59a4f5e2887973b8814e87ba35bc42e2ab68551
Pedro Franco <101286154+PedroFerreiraFranco@users.noreply.github.com>
2023-06-06 10:01:18 -0300

    Add files via upload

---
+++ Consultas Para BD/Consultas/Inserção Atividade Computer DAY.sql
@@ -0,0 +1,7 @@
+
+BEGIN;
+INSERT INTO atividade VALUES(NULL, "Criando um Chatbot", "Laboratório 1","2023-06-13 13:15", "2023-06-13 15:00",
+30,"Minicurso","William Silvestre","784.525.625-36",1); 
+COMMIT;
+
+SELECT * FROM atividade;
